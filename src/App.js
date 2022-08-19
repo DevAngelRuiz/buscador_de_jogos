154b40beb1a7e2f62c85cb26f897478b06aa0c22
DevAngelRuiz <angelica.a.ruiz@gmail.com>
2022-08-19 14:16:58 -0300

    layout ok, implementando lógica

--- src/App.js
+++ src/App.js
@@ -1,25 +1,183 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, ContainerItems, InputRam, Button, Label } from './styles'
+import api from './api'
+import { useState, useEffect } from 'react'
+import * as React from 'react';
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import Chip from '@mui/material/Chip';
+
+
+
 
 function App() {
+
+  //Select genero
+
+  const ITEM_HEIGHT = 48;
+  const ITEM_PADDING_TOP = 8;
+  const MenuProps = {
+    PaperProps: {
+      style: {
+        maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+        width: 250,
+      },
+    },
+  };
+
+  const genre = [
+    'mmorpg', 'shooter', 'strategy', 'moba', 'racing', 'sports', 'social', 'sandbox', 'open-world', 'survival', 'pvp', 'pve', 'pixel',
+    'voxel', 'zombie', 'turn-based', 'first-person', 'third-Person', 'top-down', 'tank', 'space', 'sailing', 'side-scroller', 'superhero',
+    'permadeath', 'card', 'battle-royale', 'mmo', 'mmofps', 'mmotps', '3d', '2d', 'anime', 'fantasy', 'sci - fi', 'fighting',
+    'action - rpg', 'action', 'military', 'martial - arts', 'flight', 'low - spec', 'tower - defense', 'horror', 'mmorts'
+  ]
+
+
+  function getStyles(name, generName, theme) {
+    return {
+      fontWeight:
+      generName.indexOf(name) === -1
+          ? theme.typography.fontWeightRegular
+          : theme.typography.fontWeightMedium,
+    };
+  }
+
+
+  const theme = useTheme();
+  const [genreName, setGenreName] = React.useState([]);
+
+  const handleChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setGenreName(
+      // On autofill we get a stringified value.
+      typeof value === 'string' ? value.split(',') : value,
+    
+    );
+    console.log(setGenreName)
+
+    //array vazio????
+
+   
+    
+  }
+
+
+//teste get api 
+
+  const [games, setGames] = useState([])
+
+  useEffect(() => {
+    api
+      .get('/game?id=452')
+      .then((response) => setGames(response.data))
+      .catch((err) => {
+        console.error("ops! ocorreu um erro" + err);
+      });
+  }, []);
+
+  //select opção pc ou navegador
+
+  const [option, setOption] = React.useState('');
+
+  const pcOrBrowser = (event) => {
+    setOption(event.target.value);
+    console.log(setOption)
+  };
+
+  // pegar os dados do input e exectuar o Onclick
+  
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+
+
+    <div>
+      <Container>
+        <ContainerItems>
+          <h1>Descubra seu próximo jogo</h1>
+          <Label>Escolha quantos gêneros desejar:</Label>
+
+          <FormControl sx={{ m: 1, width: 342 }} >
+            <InputLabel id="demo-multiple-chip-label">Escolha o Gênero:</InputLabel>
+            <Select
+              labelId="demo-multiple-chip-label"
+              id="demo-multiple-chip"
+              multiple
+              value={genreName}
+              onChange={handleChange}
+              input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
+              renderValue={(selected) => (
+                <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+                  {selected.map((value) => (
+                    <Chip key={value} label={value} />
+                  ))}
+                </Box>
+              )}
+              MenuProps={MenuProps}
+            >
+              {genre.map((name) => (
+                <MenuItem
+                  key={name}
+                  value={name}
+                  style={getStyles(name, genreName, theme)}
+                >
+                  {name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+
+  {/* ACEITAR APENAS NUMEROS INTEIROS */}
+
+          <Label>Quantos GB de memória ram você tem?</Label>
+          <InputRam placeholder='Apenas o número. Ex: 4' type='number' ></InputRam>
+
+          <Label>Deseja jogar no PC ou Navegador?</Label>
+          <Box sx={{ minWidth: 342 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Opcional</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={option}
+          label="option"
+          onChange={pcOrBrowser}
         >
-          Learn React
-        </a>
-      </header>
+          <MenuItem value={10}>PC</MenuItem>
+          <MenuItem value={20}>Browser</MenuItem>
+          <MenuItem value={30}>Tanto Faz</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+  
+
+          
+          <Button>Buscar</Button>
+
+
+          <p> Nome: {games?.title}</p>
+          <p> Link: {games?.game_url}</p>
+          <p> busca: {}</p>
+          <Button onClick={() => window.location.reload()}>Nova Pesquisa</Button>
+        </ContainerItems>
+      </Container>
+
+
+
     </div>
-  );
+  )
 }
 
-export default App;
+
+export default App
+
+
+
+
+
---
+++ src/styles.js
@@ -0,0 +1,129 @@
+import styled from 'styled-components'
+import Background from './assets/background.jpg'
+
+export const Container = styled.div`
+background: url("${Background}");
+background-size: cover;
+width: 100vw;
+height: 100vh;
+display: flex;
+flex-direction: column;
+align-items: center;
+gap: 40px;
+padding-top: 20px;
+
+
+`
+
+
+export const ContainerItems = styled.div`
+background: linear-gradient(157.44deg, rgba(255, 255, 255, 0.6) 0.84%, rgba(255, 255, 255, 0.6) 0.85%, rgba(255, 255, 255, 0.15) 100%);
+border-radius: 61px 61px 0px 0px;
+padding: 50px 36px;
+display: flex;
+flex-direction: column;
+backdrop-filter: blur(3px);
+height: 100vh;
+align-items: center;
+
+
+h1{
+font-style: bold;
+font-weight: 700;
+font-size: 40px;
+line-height: 40px;
+text-align: center;
+color: black;
+margin-bottom: 80px;
+}
+
+
+`
+
+
+
+export const SelectGenrer = styled.select`
+width: 342px;
+height: 58px;
+background: rgba(255, 255, 255, 0.25);
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+font-weight: 400;
+font-size: 24px;
+line-height: 28px;
+color: black;
+border: none; 
+outline: none;
+padding-left: 25px;
+margin-bottom: 34px;
+`
+
+export const Label = styled.p`
+font-style: normal;
+font-weight: bold;
+font-size: 18px;
+line-height: 22px;
+color: black;
+margin-left: 25px;
+
+`
+export const InputRam = styled.input`
+background: rgba(255, 255, 255, 0.25);
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+border-radius: 14px;
+width: 342px;
+height: 58px;
+font-weight: 400;
+font-size: 24px;
+line-height: 28px;
+color: black;
+border: none; 
+outline: none;
+padding-left: 25px;
+margin-bottom: 34px;
+::placeholder{
+    font-size: 15px;
+
+}
+`
+
+export const Selectt = styled.select`
+width: 342px;
+height: 58px;
+background: rgba(255, 255, 255, 0.25);
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+font-weight: 400;
+font-size: 24px;
+line-height: 28px;
+color: black;
+border: none; 
+outline: none;
+padding-left: 25px;
+margin-bottom: 34px;
+`
+export const Button = styled.button`
+width: 342px;
+height: 60px;
+background: rgba(0, 0, 0, 0.8);
+border-radius: 14px;
+border: none;
+margin-top: 40px;
+font-style: normal;
+font-weight: 700;
+font-size: 17px;
+line-height: 28px;
+color: #FFFFFF;
+cursor: pointer;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+
+&:hover{
+    opacity: 0.8;
+
+}
+&:active{
+    opacity: 0.5;
+}
+
+`
